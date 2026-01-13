cff6d34a2b68af61c8e1bb0a34fd80d006ca8e71
LuccaGarDal <luccagarciarosa@gmail.com>
2026-01-13 14:25:10 -0300

    first commit

---
+++ src/main/java/br/com/lucca/pet_adocao_api/PetAdocaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucca.pet_adocao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdocaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdocaoApiApplication.class, args);
+	}
+
+}
